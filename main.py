0ac6bc7bbc03aeee155586f02f1913e58bb3d46e
Dabo Ross <daboross@daboross.net>
2014-04-28 16:12:00 -0700

    Add a /oauth/ endpoint for oauth requests. It gives the user a base64 encoded json array of the GET parameters.
    Example:
    http://dabo.guru/py/oauth/?key=value
    will result in
    base64.b64encode(json.dumps({"key": "value"}))

--- main.py
+++ main.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import base64
 import json
 import os
 import traceback
@@ -37,10 +38,13 @@ device = config["pushbullet"]["device"]
 push = PushBullet(api_key)
 app = Flask(__name__)
 
+with open(os.path.join("templates", "oauth-template.html")) as oauth_template_file:
+    oauth_template = oauth_template_file.read()
+
 
 # noinspection PyBroadException
 @app.route("/notify", methods=["POST"])
-def notify():
+def notify_respond():
     try:
         print("Web notice: " + request.data.decode())
         push.push_note(device, "Web Notice", request.data.decode())
@@ -48,6 +52,17 @@ def notify():
     except Exception:
         traceback.print_exc()
         return """Failure\n"""
+
+
+# noinspection PyBroadException
+@app.route("/oauth/", methods=["GET"])
+def oauth_respond():
+    try:
+        data = base64.b64encode(json.dumps(request.args).encode()).decode()
+        return oauth_template.replace("<%content%>", data)
+    except Exception:
+        traceback.print_exc()
+        return """Something went wrong. Please contact dabo@dabo.guru about this incident.\n"""
 
 
 if __name__ == "__main__":
