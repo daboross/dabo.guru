75b7279d2ab9fa638a516e4bacdc699bea5d053c
Dabo Ross <daboross@daboross.net>
2014-04-28 15:23:37 -0700

    Change from using api-key file, to config.json config

--- main.py
+++ main.py
@@ -13,31 +13,49 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
+import os
 import traceback
+import sys
 
 from flask import Flask, request
+
 from pushbullet import PushBullet
 
-with open('api-key', 'r', encoding="UTF-8") as f:
-    global api_key
-    global device
-    api_key = f.readline().strip()
-    device = f.readline().strip()
+
+def load_config():
+    if os.path.isfile("config.json"):
+        with open("config.json") as config_file:
+            config = json.load(config_file)
+    else:
+        print("Config not found! Please copy config.default.json to config.json")
+        sys.exit()
+    return config
+
+
+def setup():
+    config = load_config()
+    global api_key, device
+    api_key = config["pushbullet"]["api-key"]
+    device = config["pushbullet"]["device"]
+
 
 push = PushBullet(api_key)
 app = Flask(__name__)
 
-# noinspection PyBroadException
+
 @app.route("/notify", methods=["POST"])
 def hello():
+    # noinspection PyBroadException
     try:
         print("Web notice: " + request.data.decode())
         push.push_note(device, "Web Notice", request.data.decode())
         return """Success\n"""
-    except:
+    except Exception:
         traceback.print_exc()
         return """Failure\n"""
 
 
 if __name__ == "__main__":
+    setup()
     app.run(host="127.0.0.1", port=5445)
