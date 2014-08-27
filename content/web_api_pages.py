55ac9c12ddc2dc00f4adb4fed277115bc21313bc
Dabo Ross <daboross@daboross.net>
2014-08-26 23:33:56 -0700

    Use display-data.html for oauth response and error, removing specific templates.

--- content/web_api_pages.py
+++ content/web_api_pages.py
@@ -7,6 +7,11 @@ from flask import request, render_template
 
 from content import app, push
 
+valid_response_title = "OAuth response"
+valid_response_desc = "Please copy the following data:"
+invalid_response_title = "Invalid OAuth response"
+invalid_response_desc = "The URL provided is invalid. Parameters oauth_token and/or oauth_verifier are missing"
+
 
 @app.route("/notify", methods=["POST"])
 def notify_respond():
@@ -19,9 +24,11 @@ def notify_respond():
 def oauth_respond():
     if "oauth_token" in request.args and "oauth_verifier" in request.args:
         data = base64.b64encode(json.dumps(request.args).encode()).decode()
-        return render_template("oauth-response.html", data=data)
+        return render_template("display-data.html", title=valid_response_title, desc=valid_response_desc,
+                               data=data, rows=2)
     else:
         data = json.dumps(request.args, indent=4)
         rows = data.count("\n") + 1
-        return render_template("oauth-error.html", data=data, rows=rows)
+        return render_template("display-data.html", title=invalid_response_title, desc=invalid_response_desc,
+                               data=data, rows=rows)
 
