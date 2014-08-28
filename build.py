3d42553ae6acd452e122a1a89851e4693a89abde
Dabo Ross <daboross@daboross.net>
2014-08-28 12:13:14 -0700

    Add workaround for frozen-flask generating static pages for dynamic api pages like /oauth/.

--- build.py
+++ build.py
@@ -12,11 +12,22 @@ for bundle in assets:
     bundle.build(force=True, disable_cache=True)
     bundle_files.append(bundle.output)
 
-print("Freezing")
+
+# This is a copy of Freezer.no_argument_rules() modified to ignore certain paths
+def no_argument_rules_urls_with_ignore():
+    """URL generator for URL rules that take no arguments."""
+    ignored = app.config.get('FREEZER_IGNORE_ENDPOINTS', [])
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint not in ignored and not rule.arguments and 'GET' in rule.methods:
+            yield rule.endpoint, {}
+
 
 app.config['FREEZER_DESTINATION'] = os.path.abspath("static")
 app.config['FREEZER_BASE_URL'] = "http://dabo.guru/"
 app.config['FREEZER_DESTINATION_IGNORE'] = bundle_files
-freezer = Freezer(app=app, with_static_files=False, with_no_argument_rules=True, log_url_for=True)
+app.config['FREEZER_IGNORE_ENDPOINTS'] = ['oauth_respond', 'mc_api_name', 'mc_api_uuid', 'serve_markdown']
+freezer = Freezer(app=app, with_static_files=False, with_no_argument_rules=False, log_url_for=True)
+freezer.register_generator(no_argument_rules_urls_with_ignore)
 
+print("Freezing")
 freezer.freeze()
