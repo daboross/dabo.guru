6e773f87810928ddcb09e57195140b2f81e6a83d
Dabo Ross <daboross@daboross.net>
2016-07-12 15:06:27 -0700

    Move statistics_key_names back into each individual file.

--- content/statistics_api.py
+++ content/statistics_api.py
@@ -4,7 +4,23 @@ import time
 from flask import request
 
 from content import app, redis
-from resources_lib.statistics_key_names import *
+
+# (plugin_name): set(plugin_version)
+PLUGIN_VERSION_LIST = "statistics:live:{}:versions"
+# (plugin_name, plugin_version): zset(server_guid: expiration_time)
+VERSION_SERVER_HASH = "statistics:live:{}:version:{}:servers"
+# (plugin_name, server_guid): hash("players": player_count, "server": server_version)
+SERVER_DATA_SET = "statistics:live:{}:server:{}:data"
+# (plugin_name): list(recorded_time)
+RECORD_LIST = "statistics:{}:record-list"
+# (plugin_name, recorded_time): hash(plugin_version: plugin_count)
+RECORD_PLUGIN_VERSION_PLUGIN_COUNTS = "statistics:{}:record:{}:version-counts"
+# (plugin_name, recorded_time): int
+RECORD_TOTAL_PLAYERS = "statistics:{}:record:{}:total-player-count"
+# (plugin_name, recorded_time): set(plugin_version)
+RECORD_PLUGIN_VERSIONS = "statistics:{}:record:{}:versions"
+# (plugin_name, recorded_time, plugin_version): hash(server_version: plugin_count)
+RECORD_SERVER_VERSION_PLUGIN_COUNTS = "statistics:{}:record:{}:version:{}:server-version-counts"
 
 
 @app.route("/statistics/v1/<plugin>/post/", methods=["POST"])
--- tasks/record_statistics.py
+++ tasks/record_statistics.py
@@ -1,11 +1,35 @@
+import logging
 import time
+import traceback
 
 from collections import Counter
 from redis import StrictRedis
 
-from resources_lib.statistics_key_names import *
+from resources_lib import configuration_resources
 
+# (plugin_name): set(plugin_version)
+PLUGIN_VERSION_LIST = "statistics:live:{}:versions"
+# (plugin_name, plugin_version): zset(server_guid: expiration_time)
+VERSION_SERVER_HASH = "statistics:live:{}:version:{}:servers"
+# (plugin_name, server_guid): hash("players": player_count, "server": server_version)
+SERVER_DATA_SET = "statistics:live:{}:server:{}:data"
+# (plugin_name): list(recorded_time)
+RECORD_LIST = "statistics:{}:record-list"
+# (plugin_name, recorded_time): hash(plugin_version: plugin_count)
+RECORD_PLUGIN_VERSION_PLUGIN_COUNTS = "statistics:{}:record:{}:version-counts"
+# (plugin_name, recorded_time): int
+RECORD_TOTAL_PLAYERS = "statistics:{}:record:{}:total-player-count"
+# (plugin_name, recorded_time): set(plugin_version)
+RECORD_PLUGIN_VERSIONS = "statistics:{}:record:{}:versions"
+# (plugin_name, recorded_time, plugin_version): hash(server_version: plugin_count)
+RECORD_SERVER_VERSION_PLUGIN_COUNTS = "statistics:{}:record:{}:version:{}:server-version-counts"
+
+configuration_resources.set_working_directory()
+configuration_resources.configure_logger("record_statistics-debug.log")
+
+push = configuration_resources.get_pushbullet()
 redis = StrictRedis()
+
 
 def record_record():
     plugin = "skywars"
@@ -67,7 +91,16 @@ def record_record():
 
 
 def main():
-    record_record()
+    try:
+        record_record()
+    except Exception:
+        logging.exception("Exception!")
+        try:
+            # since we have pushbullet, we can do exception notices :D
+            push.push_note("Python Exception", traceback.format_exc())
+        except Exception:
+            logging.exception("Exception sending exception note.")
+
 
 if __name__ == "__main__":
     main()
